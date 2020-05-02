3cb4400516974010e7dc3bb00cd080bec5f25b1e
Kason Sellers <kasonsellers15@gmail.com>
2020-05-02 17:42:32 -0400

    Developed saving reminder and creating geofence request to be triggered upon entering within a radius of 3.0 of the selected location.

--- starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
@@ -1,15 +1,20 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import android.app.PendingIntent
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingRequest
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.base.NavigationCommand
 import com.udacity.project4.databinding.FragmentSaveReminderBinding
+import com.udacity.project4.locationreminders.geofence.GeofenceBroadcastReceiver
 import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
@@ -44,18 +49,45 @@ class SaveReminderFragment : BaseFragment() {
 
         binding.saveReminder.setOnClickListener {
             val title = _viewModel.reminderTitle.value
-            val description = _viewModel.reminderDescription
+            val description = _viewModel.reminderDescription.value
             val location = _viewModel.reminderSelectedLocationStr.value
             val latitude = _viewModel.latitude.value
             val longitude = _viewModel.longitude.value
 
-            Log.e(SaveReminderFragment::class.java.simpleName, "${title}")
-            Log.e(SaveReminderFragment::class.java.simpleName, "${latitude}")
-
-//            TODO: use the user entered reminder details to:
+//            COMPLETED: use the user entered reminder details to:
 //             1) add a geofencing request
 //             2) save the reminder to the local db
+
+            if(!title.isNullOrEmpty() && !description.isNullOrEmpty() && !location.isNullOrEmpty()) {
+                // Create reminder and save it in view model
+                val reminder = ReminderDataItem(title, description, location, latitude, longitude)
+                _viewModel.saveReminder(reminder)
+
+                // Create Geofencing Request an trigger it based on entering the reminder location
+                // at a radius of default_geo_radius = 3.0
+                GeofencingRequest.Builder()
+                    .addGeofence(buildGeoFence(reminder))
+                    .setInitialTrigger(Geofence.GEOFENCE_TRANSITION_ENTER)
+                    .build()
+
+                val pendIntent = Intent(requireContext(), GeofenceBroadcastReceiver::class.java)
+                PendingIntent.getBroadcast(
+                    requireContext(),
+                    0,
+                    pendIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
+            }
         }
+    }
+
+    private fun buildGeoFence(reminder: ReminderDataItem): Geofence {
+        return Geofence.Builder()
+            .setRequestId(reminder.id)
+            .setCircularRegion(reminder.latitude!!, reminder.longitude!!, resources.getDimension(R.dimen.default_geo_radius))
+            .setExpirationDuration(Geofence.NEVER_EXPIRE)
+            .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
+            .build()
     }
 
     override fun onDestroy() {
